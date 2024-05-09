b696b25a7db9792d979ebf749593b1626f9a407c
Phuoc Le <lvphuocqt@gmail.com>
2024-05-09 11:25:42 +0700

    update

--- src/components/comments/comment-list.tsx
+++ src/components/comments/comment-list.tsx
@@ -4,23 +4,23 @@ interface CommentListProps {}
 
 // TODO: Get a list of comments from somewhere
 export default function CommentList({}: CommentListProps) {
-  const topLevelComments = comments.filter(
-    (comment) => comment.parentId === null
-  );
-  const renderedComments = topLevelComments.map((comment) => {
-    return (
-      <CommentShow
-        key={comment.id}
-        commentId={comment.id}
-        comments={comments}
-      />
-    );
-  });
+  // const topLevelComments = comments.filter(
+  //   (comment) => comment.parentId === null
+  // );
+  // const renderedComments = topLevelComments.map((comment) => {
+  //   return (
+  //     <CommentShow
+  //       key={comment.id}
+  //       commentId={comment.id}
+  //       comments={comments}
+  //     />
+  //   );
+  // });
 
   return (
     <div className="space-y-3">
-      <h1 className="text-lg font-bold">All {comments.length} comments</h1>
-      {renderedComments}
+      {/* <h1 className="text-lg font-bold">All {comments.length} comments</h1>
+      {renderedComments} */}
     </div>
   );
 }
