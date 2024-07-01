04cd16df0778740e0a8fa3df41e5199c942657a9
Phuoc Le <lvphuocqt@gmail.com>
2024-07-01 10:57:13 +0700

    login with google

--- src/auth.ts
+++ src/auth.ts
@@ -2,9 +2,12 @@ import NextAuth from "next-auth";
 import Github from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import db from "@/db";
+import Google from "@auth/core/providers/google";
 
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
 if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
   throw new Error("Missing github oauth credentials");
@@ -18,11 +21,16 @@ export const {
 } = NextAuth({
   adapter: PrismaAdapter(db),
   providers: [
-    Github({
-      clientId: GITHUB_CLIENT_ID,
-      clientSecret: GITHUB_CLIENT_SECRET,
+    // Github({
+    //   clientId: GITHUB_CLIENT_ID,
+    //   clientSecret: GITHUB_CLIENT_SECRET,
+    // }),
+    Google({
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
     }),
   ],
+  secret: "fesfsdefwsfsdfsdf",
   callbacks: {
     // Usually not needed, here we are fixing a bug in nextauth
     async session({ session, user }: any) {
